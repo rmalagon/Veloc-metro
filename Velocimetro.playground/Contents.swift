e99588c2c632864494de50b3e0ef33eaa2a5b3a1
Ricardo Adolfo Malagón Ganado <Ricardo@macbook-pro-de-ricardo.local>
2016-02-21 18:27:48 +0100

    ...

--- Velocimetro.playground/Contents.swift
+++ Velocimetro.playground/Contents.swift
@@ -10,6 +10,7 @@ Review Criteria
 ¿Se declara la clase: Auto?
 ¿La clase Auto cuenta con una instancia de la enumeración Velocidades, llamada: velocidad?
 ¿La clase Auto implementa el método: init( )?
+
 ¿La clase Auto implementa la función: func cambioDeVelocidad( ) -> ( actual : Int, velocidadEnCadena: String) y sus reglas de ejecución?
 ¿Se realiza la prueba de la clase Auto, iterando 20 veces el método cambioDeVelocidad() y sus salidas se observan en la consola?
 */
@@ -36,17 +37,44 @@ class Auto{
         self.velocidad
     }
     
+/*
     func cambioDeVelocidad() -> (actual: Int, velocidadEnCadena: String){
+        
+        let actual = velocidad.rawValue
+        var velocidadEnCadena = "\(actual)\t\(velocidad)"
+        return actual
+        
+        
         //Mostrar la velocidad actual
         //Actualizar la velocidad y devolver tupla
     }
+*/
+
+}
+
+var auto = Auto(velocidad: 5)
+auto.velocidad.rawValue
+
+
+for iteracion in 1...20{
+    
+    /*
+    if numero==0 {
+        print(numero)
+    }else if numero >= 30 && numero <= 40{
+        print("\(numero)\t Viva Swift!!!")
+    }else if numero%5 == 0{
+        print("\(numero)\t Bingo!!!")
+    }else if numero%2 == 0{
+        print("\(numero)\t par!!!")
+    }else if numero%2 != 0{
+        print("\(numero)\t impar!!!")
+    }
+    */
 }
 
 
 
-var auto = Auto(velocidad: 5)
-
-auto.velocidad.rawValue
 
 
 
